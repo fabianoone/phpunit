74d16840445db4ae4357efcfa8d2a0fb1b788abf
Fabiano Oliveira <fabiano.one@gmail.com>
2023-12-20 21:47:40 -0300

    002 Dependency injection_ inject objects that a class depends on

--- src/User.php
+++ src/User.php
@@ -20,11 +20,45 @@ class User
     public $last_name;
 
     /**
+     * User address
+     * @var string
+     */
+    public $email;
+
+    /**
+     * Mailer object
+     * @var Mailer
+     */
+    protected $mailer;
+
+    /**
+     * Set the mailer dependency
+     * 
+     * @param Mailer $mailer The Mailer object
+     */
+    public function setMailer(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
      * Get the user's full name from their first name and last name
      * @return string The user's full name
      */
     public function getFullName()
     {
         return trim("$this->first_name $this->last_name");
+    }
+
+    /**
+     * Send the user a message
+     * 
+     * @param string $message The message
+     * 
+     * @return boolean Ture if sent, false otherwise
+     */
+    public function notify($message)
+    {
+        return $this->mailer->sendMessage($this->email, $message);
     }
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -30,4 +30,19 @@ class UserTest extends TestCase
         $user->first_name = 'Teresa';
         $this->assertEquals('Teresa', $user->first_name);
     }
+
+    public function testNotificationIsSent()
+    {
+        $user = new User;
+
+        $mock_mailer = $this->createMock(Mailer::class);
+
+        $mock_mailer->method('sendMessage')->willReturn(true);
+
+        $user->setMailer($mock_mailer);
+
+        $user->email = 'fabiano@example.com';
+
+        $this->assertTrue($user->notify("Hello"));
+    }
 }
